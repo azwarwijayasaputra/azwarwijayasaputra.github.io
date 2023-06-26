2b595900168c881394006af20e75037790738b83
azwarwijayasaputra <azwar.capilkonawe@gmail.com>
2023-06-26 13:24:37 +0800

    first commit

---
+++ assets/styles/index.js
@@ -0,0 +1,7 @@
+function changeGambarstaf1(){
+    document.getElementById("karyawan").src="assets/styles/gambar/staf1.png";
+}
+function changeGambarstaf2(){
+    document.getElementById("karyawan").src="assets/styles/gambar/staf2.jpg";
+}
+alert("terima kasih telah berkunjung")
